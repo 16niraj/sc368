711637dce609687fdf4e14d2eaa183a5b49367c2
Niraj kr <24110222@iitgn.ac.in>
2024-11-15 22:01:18 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// // document.getElementById("title").innerText="Today is a good day";
+// const title=document.getElementById("title");
+// title.innerText="Hello";
+
+// let age= parseInt(prompt("What's your age?"));
+// if(age>18){
+//     title.innerText="Vote";
+//     title.style.color="green";
+
+// } else{
+//     title.innerText=" Cannot Vote";
+//     title.style.color="red";
+// }
+// // 
+
+const inputText = document.getElementById("TodoTitle");
+const list = document.getElementById("todos")
+const countdown =document.getElementsByClassName("countdown")[0];
+let initialcount=10;
+const timer = setInterval(Countdown,1000 )
+
+// function Card(Todo){
+//     return 
+// }
+function Countdown(){
+    initialcount-=1;
+    countdown.innerText= initialcount;
+    if(initialcount<0){
+        clearInterval(timer)
+    }
+
+}
+const Todos =[];
+
+function onClickSubmitButton() {
+//   console.log(inputText.value);
+  Todos.push(inputText.value)
+//   console.log(Todos)
+ inputText.value="";
+//  console.log(Todos)
+ Todos.forEach((todo)=>{
+    list.innerHTML+= "<li>" + todo + "</li>";
+ });
+}
